d1e1998de5c6cfcadde311ba1676086305fe2345
vishaag <vishaag@gmail.com>
2020-08-21 05:09:32 +0800

    🐛 bug: remove unecessary if condition in mergeHotelAndPrices.js

--- utils/mergeHotelsAndPrices.js
+++ utils/mergeHotelsAndPrices.js
@@ -1,9 +1,6 @@
 export default function mergeHotelsAndPrices(hotels, prices) {
   const objectsWithoutPrice = [];
   let mergedResult = []
-  if(!hotels) {
-    hotels = []
-  }
   hotels.map((hotel)=>{
     const pricesObj = prices.find(price => price.id === hotel.id);
     if(!pricesObj) {
