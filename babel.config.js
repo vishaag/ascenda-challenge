df364295e9c1b42fae4e2689cca3e329412a7dbf
vishaag <vishaag@gmail.com>
2020-08-21 03:38:42 +0800

    🐛 bug: add next/babel to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  "presets": ["@babel/preset-env", "@babel/preset-react"]
+  "presets": [
+    [
+      "next/babel",
+      {
+        "preset-env": {},
+        "preset-react": {},
+      }
+    ]
+  ],
+  "plugins": []
 }
